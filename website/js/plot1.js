99362836f369ea351624b54e9fb3892e7e19df3a
Jonathan Kaeser <kaeser.jonathan@gmail.com>
2020-04-21 14:59:22 +0200

    added some files

---
+++ website/js/plot1.js
@@ -0,0 +1,21 @@
+(function(window) {
+  'use strict';
+  var App = window.App || {};
+  var Plot1 = (function() {
+
+    function getDataForPlot1(callback){
+
+    }
+    getDataForPlot1(()=>{
+      
+    })
+    console.log("hello")
+
+
+    return {
+      //  playVideo:showVideo,
+    }
+  })();
+  App.Plot1 = Plot1;
+  window.App = App;
+})(window);
