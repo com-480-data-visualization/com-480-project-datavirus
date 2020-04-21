9da639c577e401f971a249919948f175d4c5bbed
Jonathan Kaeser <kaeser.jonathan@gmail.com>
2020-04-21 15:15:35 +0200

    added first css file

--- website/js/plot1.js
+++ website/js/plot1.js
@@ -1,15 +1,35 @@
 (function(window) {
   'use strict';
   var App = window.App || {};
-  var Plot1 = (function() {
+  let Plot1 = (function() {
 
-    function getDataForPlot1(callback){
+    const chartDiv = document.getElementById("plot1-container");
 
-    }
-    getDataForPlot1(()=>{
-      
-    })
+    const margin = {
+        top: 10,
+        right: 40,
+        bottom: 150,
+        left: 60
+      },
+      width = chartDiv.clientWidth * 0.95,
+      height = chartDiv.clientHeight * 0.5,
+      contextHeight = 50,
+      contextWidth = width;
+
+
+    const svg = d3.select("#plot1-container").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", (height + margin.top + margin.bottom));
+
+    d3.csv("/data/plot1data.csv",function(data) {
+      console.log(data[0]);
+    });
     console.log("hello")
+
+
+    function log(truc){
+      console.log(truc)
+    }
 
 
     return {
