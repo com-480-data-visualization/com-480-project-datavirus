adf5709402a9e08f4f67eeb887b89058918dd277
Jonathan Kaeser <kaeser.jonathan@gmail.com>
2020-05-10 13:24:40 +0200

    added youtubeAPI ready to use

--- website/js/youtubeAPI.js
+++ website/js/youtubeAPI.js
@@ -2,11 +2,43 @@
   'use strict';
   var App = window.App || {};
   let YoutubeAPI = (function() {
+    const API_Key = "AIzaSyB0fx0AqSe-gzktQ4q9hbWQ5T1CBWDMnyo"
 
-    console.log("öLjélj")
+    function getMetaDataForVideo(videoId, callback){
+      let url = "https://www.googleapis.com/youtube/v3/videos?id="+videoId+"&key="
+      +API_Key+"&fields=items(id,snippet(publishedAt,channelId,title,description,thumbnails(medium(url))),statistics)&part=snippet,statistics"
+      ajaxGet(url,callback)
+    }
+
+
+    function ajaxGet(url, callback) {
+      var req = new XMLHttpRequest();
+      req.open("GET", url);
+      req.addEventListener("load", function () {
+        if (req.status >= 200 && req.status < 400) {
+          // Appelle la fonction callback en lui passant la réponse de la requête
+          callback(req.responseText);
+        } else {
+          console.error(req.status + " " + req.statusText + " " + url);
+        }
+      });
+      req.addEventListener("error", function () {
+        console.error("Erreur réseau avec l'URL " + url);
+      });
+      req.send(null);
+    }
+
+
+    /*
+    //Example of use:
+    getMetaDataForVideo("7lCDEYXw3mM", function(result){
+    console.log(result)
+  })
+  */
+
 
   return {
-
+    getMetaDataForVideo:getMetaDataForVideo,
   }
 })();
 App.YoutubeAPI = YoutubeAPI;
