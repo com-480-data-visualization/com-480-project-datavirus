70b3cf55f71e14d11704f1c21263d78e7a2265d2
Jonathan Kaeser <kaeser.jonathan@gmail.com>
2020-05-10 12:00:23 +0200

    added new file

---
+++ website/js/youtubeAPI.js
@@ -0,0 +1,14 @@
+(function(window) {
+  'use strict';
+  var App = window.App || {};
+  let YoutubeAPI = (function() {
+
+    console.log("öLjélj")
+
+  return {
+
+  }
+})();
+App.YoutubeAPI = YoutubeAPI;
+window.App = App;
+})(window);
